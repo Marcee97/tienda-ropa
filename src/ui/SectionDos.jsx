63774e02dfad6bc77df368a90f5004a0aa8b3ea8
Marce Quadrilatero <marceloquadrilatero@gmail.com>
2024-07-24 14:11:04 -0300

    arreglo de seccion

--- src/ui/SectionDos.jsx
+++ src/ui/SectionDos.jsx
@@ -3,33 +3,39 @@ import "../styles/sectiondos.css";
 
 export const SectionDos = () => {
   const elementoDos = useRef(null);
-  const elementoTres = useRef(null)
-
- 
+  const elementoImg = useRef(null);
 
   useEffect(() => {
     const ubicacion = 1556;
+    const ubicacionImg = 1812;
 
     const funScrollUp = () => {
       const scrollUp = window.scrollY;
 
       if (scrollUp >= ubicacion) {
-        const indexUp = Math.floor((scrollUp - ubicacion) / 5.491);
-        console.log(indexUp, "indexUp");
+        const indexUp = Math.floor((scrollUp - ubicacion) / 5.526);
         const numeroFinal = Math.min(100, indexUp);
 
-        console.log(numeroFinal, "numerofinañ");
 
         elementoDos.current.style.transform = `translateY(-${numeroFinal}%)`;
-       
-
-      }else if(scrollUp < ubicacion){
-
-        if(elementoDos.current) {
-          elementoDos.current.style.transform = 'translateY(0)'
+      } else if (scrollUp < ubicacion) {
+        if (elementoDos.current) {
+          elementoDos.current.style.transform = "translateY(0)";
         }
       }
+
+      if(scrollUp >= ubicacionImg){
+        const indexImg = Math.floor((scrollUp - ubicacionImg) / 3)
+        console.log(indexImg, 'Mov Igm')
+
+        const indexImgFinal = Math.min(100, indexImg)
+        elementoImg.current.style.transform = `translateY(-${indexImgFinal}%)`
+      }else if(scrollUp < ubicacionImg){
+        elementoImg.current.style.transform = 'translateY(0)'
+      }
     };
+
+  
 
     window.addEventListener("scroll", funScrollUp);
     return () => {
@@ -40,23 +46,42 @@ export const SectionDos = () => {
   return (
     <section className="section-segunda">
       <div className="cont-img">
-        
-       
-       <div className="div-img-1">
-        <img src="https://images.pexels.com/photos/19220720/pexels-photo-19220720/free-photo-of-linda-modelo-posando-con-ropa-occidental.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="Foto-muestra" className="img-1"/>
 
-        <p className="text-desplazado-imagen" ref={elementoDos}></p>
 
-       </div>
+        <div className="div-img-0">
+      <img src="https://i.pinimg.com/564x/2b/b5/21/2bb521d4cc9d3a59ac6a3b1bfe210a1a.jpg" alt="imagen estatica" className="img-0" />
+
+      <img src="https://i.pinimg.com/564x/b0/ab/17/b0ab17f34ab317cf6d094301ba1696c1.jpg" alt="imagen scroll" className="img-0-scroll" ref={elementoImg} />
+        </div>
+
+
+
+
+        <div className="div-img-1">
+          <img
+            src="https://images.pexels.com/photos/19220720/pexels-photo-19220720/free-photo-of-linda-modelo-posando-con-ropa-occidental.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt="Foto-muestra"
+            className="img-1"
+          />
+
+          <p className="text-desplazado-imagen" ref={elementoDos}></p>
+        </div>
       </div>
-      
-      <div className="cont-moving" ref={elementoTres}>
-        <article className="titulo-y-texto">
 
-<h3 className="cont-moving-text">About..</h3>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, dicta. Quos, error, consequuntur nostrum cum ea quam modi dolorem laboriosam, asperiores quasi dolore nulla minus inventore amet sit placeat accusantium. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit veritatis ullam, a enim tempore unde maxime, accusantium in, atque necessitatibus dolorem ea perferendis ratione! Voluptates maiores praesentium repellat ipsam natus.</p>
+      <div className="cont-moving" >
+        <article className="titulo-y-texto">
+          <h3 className="cont-moving-text">About..</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+            dicta. Quos, error, consequuntur nostrum cum ea quam modi dolorem
+            laboriosam, asperiores quasi dolore nulla minus inventore amet sit
+            placeat accusantium. Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Fugit veritatis ullam, a enim tempore unde maxime,
+            accusantium in, atque necessitatibus dolorem ea perferendis ratione!
+            Voluptates maiores praesentium repellat ipsam natus.
+          </p>
         </article>
-</div>
+      </div>
     </section>
   );
 };
