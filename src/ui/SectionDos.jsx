483579285155329255b902e926b8eeea647651ff
Marce Quadrilatero <marceloquadrilatero@gmail.com>
2024-07-26 01:01:18 -0300

    elementosSectionShop

--- src/ui/SectionDos.jsx
+++ src/ui/SectionDos.jsx
@@ -7,32 +7,31 @@ export const SectionDos = () => {
   const elementoText = useRef(null);
 
   useEffect(() => {
-    const ubicacion = 1556;
-    const ubicacionImg = 1813;
+    const ubicacion = 1548;
+    const ubicacionImg = 1784;
     const ubicacionText = 1000;
 
     const funScrollUp = () => {
       const scrollUp = window.scrollY;
 
       if (scrollUp >= ubicacion) {
-        const indexUp = Math.floor((scrollUp - ubicacion) / 5.526);
-        const numeroFinal = Math.min(100, indexUp);
-
-        elementoDos.current.style.transform = `translateY(-${numeroFinal}%)`;
+        const indexUp = Math.floor((scrollUp - ubicacion) / 5.43);
+        const numeroFinal = Math.max(0, 100 - indexUp);
+        console.log(numeroFinal, "indexheigth");
+        elementoDos.current.style.height = `${numeroFinal}%`;
       } else if (scrollUp < ubicacion) {
         if (elementoDos.current) {
-          elementoDos.current.style.transform = "translateY(0)";
+          elementoDos.current.style.height = "100%";
         }
       }
 
       if (scrollUp >= ubicacionImg) {
-        const indexImg = Math.floor((scrollUp - ubicacionImg) / 2.98);
-        console.log(indexImg, "Mov Igm");
+        const indexImg = Math.floor((scrollUp - ubicacionImg) / 2.94);
 
-        const indexImgFinal = Math.min(100, indexImg);
-        elementoImg.current.style.transform = `translateY(-${indexImgFinal}%)`;
+        const indexImgFinal = Math.max(0, 100 - indexImg);
+        elementoImg.current.style.height = `${indexImgFinal}%`;
       } else if (scrollUp < ubicacionImg) {
-        elementoImg.current.style.transform = "translateY(0)";
+        elementoImg.current.style.height = "100%";
       }
 
       if (scrollUp >= ubicacionText) {
@@ -58,46 +57,39 @@ export const SectionDos = () => {
         <article className="cont-img-article">
           <div className="div-img-0">
             <img
-              src="https://i.pinimg.com/564x/2b/b5/21/2bb521d4cc9d3a59ac6a3b1bfe210a1a.jpg"
+              src="https://i.pinimg.com/564x/33/69/8b/33698b65b6f94d7fc2f35b56b38b1c1b.jpg"
               alt="imagen estatica"
               className="img-0"
             />
-
-            <img
-              src="https://i.pinimg.com/564x/b0/ab/17/b0ab17f34ab317cf6d094301ba1696c1.jpg"
-              alt="imagen scroll"
-              className="img-0-scroll"
-              ref={elementoImg}
-            />
+            <div className="cont-img-0-scroll" ref={elementoImg}>
+              <img
+                src="https://i.pinimg.com/564x/d5/9c/13/d59c135860dcdd78edb8fd9c152cf694.jpg"
+                alt="imagen scroll"
+                className="img-0-scroll"
+              />
+            </div>
           </div>
         </article>
 
         <div className="div-img-1">
           <img
-            src="https://images.pexels.com/photos/19220720/pexels-photo-19220720/free-photo-of-linda-modelo-posando-con-ropa-occidental.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            src="https://i.pinimg.com/564x/99/60/bc/9960bc2649da9884965ad4c77e1589b4.jpg"
             alt="Foto-muestra"
             className="img-1"
           />
-
-          <p className="text-desplazado-imagen" ref={elementoDos}></p>
+          <div className="cont-texto-deplazado-img" ref={elementoDos}>
+            <p className="text-desplazado-img">We..</p>
+          </div>
         </div>
       </div>
 
       <div className="cont-moving">
         <article className="titulo-y-texto">
           <h3 className="cont-moving-text" ref={elementoText}>
-            Woman
+            Remeras
           </h3>
-
-          <p className="cont-moving-parrafo-woman">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
-            exercitationem enim quis perspiciatis suscipit aliquam voluptatibus
-            commodi quia nihil fugit tempore id saepe quod reiciendis laborum
-            unde, odit aliquid provident.
-          </p>
         </article>
       </div>
-     
     </section>
   );
 };
--- src/ui/SectionShop.jsx
+++ src/ui/SectionShop.jsx
@@ -1,13 +1,50 @@
-import '../styles/sectionshop.css'
-
-
-
-
+import "../styles/sectionshop.css";
+import { useEffect, useRef } from "react";
 
 export const SectionShop = () => {
+  const productoRef = useRef(null);
+  const hoverProductoRef = useRef(null);
+  useEffect(() => {
+    const productoCurrentRef = productoRef.current;
+    const hoverProductoCurrentRef = hoverProductoRef.current;
+
+    const mouseEnter = () => {
+      hoverProductoCurrentRef.style.visibility = "visible";
+    };
+    const mouseLeave = () => {
+      hoverProductoCurrentRef.style.visibility = "hidden";
+
+    };
+
+    productoCurrentRef.addEventListener("mouseenter", mouseEnter);
+    productoCurrentRef.addEventListener("mouseleave", mouseLeave);
+
+    return () => {
+      productoCurrentRef.removeEventListener("mouseenter", mouseEnter);
+      productoCurrentRef.removeEventListener("mouseleave", mouseLeave);
+    };
+  }, []);
+
   return (
     <section className="section-shop">
+      <div className="contenedor-productos">
+        <h4 className="cont-ropa-titulo">Section Ropa</h4>
 
+        <div className="contenedor-img">
+          <div className="div-producto" ref={productoRef}>
+            <img
+              src="/img/imgproducto(1).png"
+              alt="foto producto"
+              className="img-producto-shop"
+            />
+            <article className="article-producto" ref={hoverProductoRef}>
+              <h5 className="info-producto">Remera Estilo Run Park</h5>
+              <p></p>
+              <strong className="precio-producto">$2.900</strong>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
